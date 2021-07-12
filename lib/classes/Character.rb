7e02e88bbbaacc37b60cf2e622f48cdc0a619363
Lee Reynolds <lxreynolds@godaddy.com>
2021-07-12 00:17:56 -0700

    Calculations for PTP, MTP, stat growth, and experience

--- lib/classes/Character.rb
+++ lib/classes/Character.rb
@@ -110,38 +110,57 @@ class Character
 
   # Calculate PTP/MTP. This formula is (STR + CON + DEX + AGI) + (AUR + DIS) / 2  for PTP and (LOG + INT + WIS + INF) + (AUR + DIS) / 2 for MTP.
   # Prime statistics count as double for these calculations
-  def getPTP
+  def getPtp_by_stats(myAur, myDis, myStr, myCon, myDex, myAgi)
     phsStats = getProfession[:prime]
 
-    aur = (phsStats[:stat1] == 'Aura'         || phsStats[:stat2] == 'Aura')         ? @stats.getAur() * 2 : @stats.getAur()
-    dis = (phsStats[:stat1] == 'Discipline'   || phsStats[:stat2] == 'Discipline')   ? @stats.getDis() * 2 : @stats.getDis()
-    str = (phsStats[:stat1] == 'Strength'     || phsStats[:stat2] == 'Strength')     ? @stats.getStr() * 2 : @stats.getStr()
-    con = (phsStats[:stat1] == 'Constitution' || phsStats[:stat2] == 'Constitution') ? @stats.getCon() * 2 : @stats.getCon()
-    dex = (phsStats[:stat1] == 'Dexterity'    || phsStats[:stat2] == 'Dexterity')    ? @stats.getDex() * 2 : @stats.getDex()
-    agi = (phsStats[:stat1] == 'Agility'      || phsStats[:stat2] == 'Agility')      ? @stats.getAgi() * 2 : @stats.getAgi()
+    aur = (phsStats[:stat1] == 'Aura' || phsStats[:stat2] == 'Aura') ? myAur * 2 : myAur
+    dis = (phsStats[:stat1] == 'Discipline' || phsStats[:stat2] == 'Discipline') ? myDis * 2 : myDis
+    str = (phsStats[:stat1] == 'Strength' || phsStats[:stat2] == 'Strength') ? myStr * 2 : myStr
+    con = (phsStats[:stat1] == 'Constitution' || phsStats[:stat2] == 'Constitution') ? myCon * 2 : myCon
+    dex = (phsStats[:stat1] == 'Dexterity' || phsStats[:stat2] == 'Dexterity') ? myDex * 2 : myDex
+    agi = (phsStats[:stat1] == 'Agility' || phsStats[:stat2] == 'Agility') ? myAgi * 2 : myAgi
 
-    ptp_sum = (aur + dis)/2
+    ptp_sum = (aur + dis) / 2
     ptp_sum = (ptp_sum + str + con + dex + agi) / 20
     ptp_sum += 25
 
     return ptp_sum
   end
 
-  def getMTP
+  def getPTP
+    ptp_sum = getPtp_by_stats(@stats.getAur(), @stats.getDis(), @stats.getStr(), @stats.getCon(), @stats.getDex(), @stats.getAgi())
+    return ptp_sum
+  end
+
+  def getMtp_by_stats(myAur, myDis, myLog, myInt, myWis, myInf)
     menStats = getProfession[:mana]
 
-    aur = (menStats[:stat1] == 'Aura'       || menStats[:stat2] == 'Aura')       ? @stats.getAur() * 2 : @stats.getAur()
-    dis = (menStats[:stat1] == 'Discipline' || menStats[:stat2] == 'Discipline') ? @stats.getDis() * 2 : @stats.getDis()
-    log = (menStats[:stat1] == 'Logic'      || menStats[:stat2] == 'Logic')      ? @stats.getLog() * 2 : @stats.getLog()
-    int = (menStats[:stat1] == 'Intuition'  || menStats[:stat2] == 'Intuition')  ? @stats.getInt() * 2 : @stats.getInt()
-    wis = (menStats[:stat1] == 'Wisdom'     || menStats[:stat2] == 'Wisdom')     ? @stats.getWis() * 2 : @stats.getWis()
-    inf = (menStats[:stat1] == 'Influence'  || menStats[:stat2] == 'Influence')  ? @stats.getInf() * 2 : @stats.getInf()
+    aur = (menStats[:stat1] == 'Aura' || menStats[:stat2] == 'Aura') ? myAur * 2 : myAur
+    dis = (menStats[:stat1] == 'Discipline' || menStats[:stat2] == 'Discipline') ? myDis * 2 : myDis
+    log = (menStats[:stat1] == 'Logic' || menStats[:stat2] == 'Logic') ? myLog * 2 : myLog
+    int = (menStats[:stat1] == 'Intuition' || menStats[:stat2] == 'Intuition') ? myInt * 2 : myInt
+    wis = (menStats[:stat1] == 'Wisdom' || menStats[:stat2] == 'Wisdom') ? myWis * 2 : myWis
+    inf = (menStats[:stat1] == 'Influence' || menStats[:stat2] == 'Influence') ? myInf * 2 : myInf
 
-    mtp_sum = (aur + dis)/2
+    mtp_sum = (aur + dis) / 2
     mtp_sum = (mtp_sum + log + int + wis + inf) / 20
     mtp_sum += 25
 
     return mtp_sum
+  end
+
+  def getMTP
+    mtp_sum = getPtp_by_stats(@stats.getAur(), @stats.getDis(), @stats.getLog(), @stats.getInt(), @stats.getWis(), @stats.getInf())
+    return mtp_sum
+  end
+
+  def getExperienceByLevel(level)
+    @experience = [   25,   25,   50,   75,  100,  125,  150,  175,  200,  225,  250,  275,  300,  325,  350,  370,  390,  410,  430,  450,  470,  490,  510,  530,  550,
+                     565,  580,  595,  610,  625,  640,  655,  670,  685,  700,  715,  730,  745,  760,  775,  785,  795,  805,  815,  825,  835,  845,  855,  865,  875,
+                     880,  885,  890,  895,  900,  905,  910,  915,  920,  925,  930,  935,  940,  945,  950,  955,  960,  965,  970,  975,  980,  985,  990,  995, 1000,
+                    1005, 1010, 1015, 1020, 1025, 1030, 1035, 1040, 1045, 1050, 1055, 1060, 1065, 1070, 1075, 1080, 1085, 1090, 1095, 1100, 1105, 1110, 1115, 1120, 1125]
+
+    return @experience[level] * 100
   end
 
   def calcHealth(level)
@@ -158,6 +177,57 @@ class Character
 
   def calcSpirit(level)
     return 0
+  end
+
+  def calcGrowth(stat)
+    myStats = Array.new()
+
+    case stat
+    when 'str'
+      growth_interval = @profession[:growth].getStr + @race[:adjust].getStr
+      myStart = @stats.getStr
+    when 'con'
+      growth_interval = @profession[:growth].getCon + @race[:adjust].getCon
+      myStart = @stats.getCon
+    when 'dex'
+      growth_interval = @profession[:growth].getDex + @race[:adjust].getDex
+      myStart = @stats.getDex
+    when 'agi'
+      growth_interval = @profession[:growth].getAgi + @race[:adjust].getAgi
+      myStart = @stats.getAgi
+    when 'dis'
+      growth_interval = @profession[:growth].getDis + @race[:adjust].getDis
+      myStart = @stats.getDis
+    when 'aur'
+      growth_interval = @profession[:growth].getAur + @race[:adjust].getAur
+      myStart = @stats.getAur
+    when 'log'
+      growth_interval = @profession[:growth].getLog + @race[:adjust].getLog
+      myStart = @stats.getLog
+    when 'int'
+      growth_interval = @profession[:growth].getInt + @race[:adjust].getInt
+      myStart = @stats.getInt
+    when 'wis'
+      growth_interval = @profession[:growth].getWis + @race[:adjust].getWis
+      myStart = @stats.getWis
+    when 'inf'
+      growth_interval = @profession[:growth].getInf + @race[:adjust].getInf
+      myStart = @stats.getInf
+    end
+
+    myStats[0] = myStart
+
+    # todo: This could need some debugging down the road..
+    #   Plan to do extensive testing against calculator at url
+    #   https://web.archive.org/web/20190605204233/https://gs4chart.cfapps.io/
+    for i in 1..100
+      myGrowth = myStats[i - 1] / growth_interval
+      myGrowth = (myGrowth <= 1) ? 1 : myGrowth
+
+      myStats[i] = (i % myGrowth == 0) ?  myStats[i - 1] + 1 : myStats[i - 1]
+    end
+
+    return myStats
   end
 
   # Setters
