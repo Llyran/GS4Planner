c33c0268609325e5b55fef43a387d0bd8d7d2958
Lee Reynolds <lxreynolds@godaddy.com>
2021-10-19 21:31:49 -0700

    Scrolling stats display cleanup

--- pages/misc_page.rb
+++ pages/misc_page.rb
@@ -4,12 +4,8 @@ class MiscPage < FXMainWindow
   end
 
   def show_page(form)
-    tab = FXTable.new(form, :opts => LAYOUT_FILL | LAYOUT_CENTER_X)
-    tab.setTableSize(10,10)
-    tab.setColumnWidth(3, 275)
-
-    # form = FXMatrix.new(form, 2, :opts => MATRIX_BY_COLUMNS | LAYOUT_FILL_X)
-    # lbl = FXLabel.new(form, "Misc page:")
-    # lbl.font = FXFont.new(@app, "Arial", 24)
+    form = FXMatrix.new(form, 2, :opts => MATRIX_BY_COLUMNS | LAYOUT_FILL_X)
+    lbl = FXLabel.new(form, "Misc page:")
+    lbl.font = FXFont.new(@app, "Arial", 24)
   end
 end
